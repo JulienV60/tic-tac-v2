3d75bae8eda2b62857a70eef3fc72c2d8bbd004c
Villalpando Julien <villalpando.julien@gmail.com>
2022-04-12 01:07:00 +0200

    Responsive accueil et Y collab/manager

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
 import LoginIcon from "@mui/icons-material/Login";
 const Home: NextPage = () => {
-  console.log("HelloWorld");
   return (
     <div>
       <div className="BodyHomePage">
         <div className="imgIndex">
           <img src="/undraw_Time_management_re_tk5w (2).png"></img>
-        </div>{" "}
+        </div>
         <div className="AsideRight">
           <h1>Welcome, to the new Tac-Tic.</h1>
           <form action="/api/auth/login" method="GET">
--- pages/test.tsx
+++
@@ -1,6 +0,0 @@
-import App from "../components/calendar";
-
-export default function test() {
-  console.log("coucou");
-  return <App />;
-}
