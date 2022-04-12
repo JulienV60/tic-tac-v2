4a506fb1a5d54b8d1e873375ecd9f088d055d0dd
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-04-12 19:58:11 +0200

    add layout and react boostrap to leave page collaborator

--- pages/collaborateur/conges.tsx
+++ pages/collaborateur/conges.tsx
@@ -7,56 +7,59 @@ import {
   SegmentedItem,
 } from "@mobiscroll/react";
 import { NextPage } from "next";
-import mobiscroll from "@mobiscroll/react";
+import { Card, Button } from "react-bootstrap";
+import LayoutCollab from "../../components/LayoutCollab";
 
 const conges: NextPage = () => {
   return (
     <>
-      <div className="container">
-        <section className="leave-section">
-          <div className="leave-title">
-            <h3 className="leave-h3">Soldes des congés payés</h3>
-          </div>
-          <div className="container-picker">
-            <p>Calendar(icone calendar) </p>
-            <span className="leave-picker ">
-              <Datepicker
-                select="range"
-                rangeHighlight={true}
-                showRangeLabels={true}
-              />
-            </span>
-            <button>Valider</button>
-          </div>
+      <LayoutCollab>
+        <div className="container">
+          <section className="leave-section">
+            <div className="leave-title">
+              <h3 className="leave-h3">Soldes des congés payés</h3>
+            </div>
+            <Card.Img
+              style={{ width: "5rem", height: "5rem" }}
+              src="/calendar.png"
+            />
+            <div className="container-picker">
+              <span className="leave-picker ">
+                <Datepicker
+                  select="range"
+                  rangeHighlight={true}
+                  showRangeLabels={true}
+                />
+              </span>
+              <Button variant="secondary">Valider</Button>
+            </div>
 
-          <div className="leave-history">
-            <div className="libelle">Droits</div>
-            <div className="start"> Pris</div>
-            <div className="end">Soldes</div>
-            <div className="quantity">Demandes en cours</div>
-            <div className="rest">Demandes accept. ou validées</div>
-            <div className="forecast-balances">Soldes prévitionnels</div>
-          </div>
-          <div className="leave-history"></div>
-          <div>Congés Payés</div>
-          <div>Congés du </div>
-        </section>
+            <div className="leave-history">
+              <div className="libelle">Droits</div>
+              <div className="start"> Pris</div>
+              <div className="end">Soldes</div>
+              <div className="quantity">Demandes en cours</div>
+              <div className="rest">Demandes accept. ou validées</div>
+              <div className="forecast-balances">Soldes prévitionnels</div>
+            </div>
+          </section>
 
-        <section className="leave-section">
-          <div className="leave-title">
-            <h3 className="leave-h3">Historiques des demandes</h3>
-          </div>
+          <section className="leave-section">
+            <div className="leave-title">
+              <h3 className="leave-h3">Historiques des demandes</h3>
+            </div>
 
-          <div className="leave-history">
-            <div className="libelle">Libellé</div>
-            <div className="start"> Date de Début</div>
-            <div className="end">Date de fin</div>
-            <div className="quantity">Quantité</div>
-            <div className="rest">Repos</div>
-            <div className="forecast-balances">Soldes prévisionnels</div>
-          </div>
-        </section>
-      </div>
+            <div className="leave-history">
+              <div className="libelle">Libellé</div>
+              <div className="start"> Date de Début</div>
+              <div className="end">Date de fin</div>
+              <div className="quantity">Quantité</div>
+              <div className="rest">Repos</div>
+              <div className="forecast-balances">Soldes prévisionnels</div>
+            </div>
+          </section>
+        </div>
+      </LayoutCollab>
     </>
   );
 };
