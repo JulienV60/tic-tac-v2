3b54c68645f1cb3e0d01a35516853a13e13194f3
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-04-12 16:19:33 +0200

    kikela wip

--- pages/collaborateur/kikela.tsx
+++ pages/collaborateur/kikela.tsx
@@ -5,28 +5,30 @@ import { getDatabase } from "../../src/database";
 import { GetServerSideProps } from "next";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const accessTokken = context.req.cookies.idTokken;
+  const accessTokken = context;
+  console.log("======================ACCESSTOKKEN==================="+ accessTokken);
+
   let profile;
-  if (context.req.cookies.idTokken === undefined) {
+
+  if (context.req.cookies.idToken === undefined) {
     profile = null;
-  } else {
-    const decoded: any = jwt_decode(accessTokken);
-    profile = await userProfil(decoded.email);
   }
 
-  const decoded: any = jwt_decode(accessTokken);
-  profile = await userProfil(decoded.email);
-  const mongodb = await getDatabase();
-  const userInfo = await mongodb
-    .db()
-    .collection("users")
-    .findOne({ email: decoded.email })
-    .then((result) => result);
+  // const decoded: any = jwt_decode(accessTokken);
+  // profile = await userProfil(decoded.email);
+  console.log("===================PROFILE===================="+profile);
+
+  // const mongodb = await getDatabase();
+  // // const userInfo = await mongodb
+  //   .db()
+  //   .collection("users")
+  //   .findOne({ email: decoded.email })
+  //   .then((result) => result);
 
   return {
     props: {
       category: profile,
-      user: JSON.stringify(userInfo),
+      user: JSON.stringify("userInfo"),
     },
   };
 };
@@ -34,44 +36,53 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 export default function Kikela(props: any) {
   const data = JSON.parse(props.user);
   return (
-    <div>
-      <div>
-        <form className="form-inline" action="/action_page.php">
-          <label>Matricule:</label>
-          <input
-            type="text"
-            id="matricule"
-            placeholder="Entrer votre matricule"
-            name="matricule"
-            value={data.matricule}
-          />
-          <label>Nom/Prenom:</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Entrer votre Nom/Prenom"
-            name="name"
-          />
-          <label>Secteur:</label>
-
-          <select name="pets" id="pet-select">
-            <option value="">--Choisissez un secteur--</option>
-            <option value="bois">Menuiserie</option>
-            <option value="sol">Sol</option>
-            <option value="paint">Peinture</option>
-            <option value="elect">Éléctricité</option>
-            <option value="sanitaire">Sanitaire</option>
-          </select>
-          <button type="submit">Submit</button>
+    <div className="kikelaInfo">
+      <div className="div1">
+        <form>
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Entrer votre matricule"
+                name="matricule"
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Entrer votre nom"
+                name="name"
+              />
+            </div>
+            <div className="col">
+              <select
+                className="form-select"
+                aria-label="Selectionnez un secteur"
+              >
+                <option selected>Selectionnez un secteur</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div className="col">
+              <button type="button" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </div>
         </form>
       </div>
 
-      <div>
-        <h3>Nom:</h3>
-        <h3>Prénom:</h3>
-        <h3>Magasin:</h3>
-        <h3>Secteur:</h3>
-        <h3>Horaire:</h3>
+      <div className="container p-5 my-5 border">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Matricule:</li>
+          <li className="list-group-item">Nom:</li>
+          <li className="list-group-item">Magasin:</li>
+          <li className="list-group-item">Secteur:</li>
+        </ul>
       </div>
     </div>
   );
