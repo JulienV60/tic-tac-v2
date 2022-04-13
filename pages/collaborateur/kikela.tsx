c02a7354ec00c95ad2b3f61bb2aa9e1fb7389731
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-04-13 10:30:21 +0200

    clean kikela

--- pages/collaborateur/kikela.tsx
+++ pages/collaborateur/kikela.tsx
@@ -60,9 +60,7 @@ export default function Kikela({ user }: any) {
                     aria-label="Selectionnez un secteur"
                   >
                     <option selected>Selectionnez un secteur</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <option value="1">Menuiserie</option>
                   </select>
                 </div>
                 <div className="col">
