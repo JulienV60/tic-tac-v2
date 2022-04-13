a9ea85500c073c9c0e8977285da038593856d3db
mansuy maxime <mmansuy@norauto.fr>
2022-04-13 15:08:24 +0200

    protect roots

--- pages/collaborateur/kikela.tsx
+++ pages/collaborateur/kikela.tsx
@@ -10,23 +10,37 @@ import { Layout } from "../../components/LayoutCollab";
 import moment from "moment";
 import { AnyError } from "mongodb";
 import { PrecisionManufacturing } from "@mui/icons-material";
+import jwt_decode from "jwt-decode";
+import PageNotFound from "../../components/PageNotFound";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const test = moment().locale("fr").format("YYYY-MM-DD");
+const accessTokken = context.req.cookies.IdToken;
+  let profile;
+  let decoded:any;
+  if (accessTokken === undefined) {
+    profile = null;
+  } else {
+    decoded = jwt_decode(accessTokken);
+    profile = await userProfil(decoded.email);
+  }
 
-  const fetchCookie = await fetch(
-    `${process.env.AUTH0_LOCAL}/api/cookies`
-  ).then((data) => data.json());
-
-  return {
-    props: {
-      user: JSON.stringify(fetchCookie),
-    },
-  };
+  if (profile === "Collaborateur") {
+    return {
+      props: {
+        profileUser: profile,
+      },
+    };
+  } else {
+    return {
+      props: {
+        profileUser: null,
+      },
+    };
+  }
 };
 
-export default function Kikela({ user }: any) {
-  const data = JSON.parse(user);
+export default function Kikela(props:any) {
+
   const [prenom, setPrenom] = React.useState("");
   const [nom, setNom] = React.useState("");
   const [dispo, setDispo] = React.useState("");
@@ -48,57 +62,61 @@ export default function Kikela({ user }: any) {
     }
   };
 
-  return (
-    <div>
-      <Layout>
-        <div className="kikelaInfo">
-          <div className="div1">
-            <form>
-              <div className="row">
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Entrer votre nom"
-                    name="nom"
-                    onChange={(event) => {
-                      setNom(event.target.value);
-                    }}
-                  />
+  if (props.profileUser === "Collaborateur") {
+    return (
+      <div>
+        <Layout>
+          <div className="kikelaInfo">
+            <div className="div1">
+              <form>
+                <div className="row">
+                  <div className="col">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Entrer votre nom"
+                      name="nom"
+                      onChange={(event) => {
+                        setNom(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="col">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Entrer votre Prenom"
+                      name="prenom"
+                      onChange={(event) => {
+                        setPrenom(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="col">
+                    <button
+                      onClick={handleSubmit}
+                      type="button"
+                      className="btn btn-primary"
+                    >
+                      Submit
+                    </button>
+                  </div>
                 </div>
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Entrer votre Prenom"
-                    name="prenom"
-                    onChange={(event) => {
-                      setPrenom(event.target.value);
-                    }}
-                  />
-                </div>
-                <div className="col">
-                  <button
-                    onClick={handleSubmit}
-                    type="button"
-                    className="btn btn-primary"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+              </form>
+            </div>
 
-          <div className="container p-5 my-5 border">
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">Nom: {nom}</li>
-              <li className="list-group-item">Prénom: {prenom}</li>
-              <li className="list-group-item">Dispo: {dispo}</li>
-            </ul>
+            <div className="container p-5 my-5 border">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Nom: {nom}</li>
+                <li className="list-group-item">Prénom: {prenom}</li>
+                <li className="list-group-item">Dispo: {dispo}</li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </Layout>
-    </div>
-  );
+        </Layout>
+      </div>
+    );
+  } else {
+    return <PageNotFound/>
+  }
 }
--- pages/collaborateur/pointages.tsx
+++ pages/collaborateur/pointages.tsx
@@ -7,191 +7,224 @@ setOptions({
   theme: "ios",
   themeVariant: "light",
 });
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { Layout } from "../../components/LayoutCollab"
+import { userProfil } from "../../src/userInfos";
+import jwt_decode from "jwt-decode";
+import PageNotFound from "../../components/PageNotFound";
 
-const pointages: NextPage = () => {
-  return (
-    <div>
-      <Layout />
-      <form action="" method="POST" className="form-example-pointages">
-        <div className="form-example">
-          <label className="LabelPointages">
-            Semaine
-            <Datepicker
-              controls={["calendar"]}
-              select="preset-range"
-              firstSelectDay={1}
-              selectSize={7}
-              display="anchored"
-            />
-          </label>
-          {/* <input
+export const getServerSideProps: GetServerSideProps = async (context) => {
+const accessTokken = context.req.cookies.IdToken;
+  let profile;
+  let decoded:any;
+  if (accessTokken === undefined) {
+    profile = null;
+  } else {
+    decoded = jwt_decode(accessTokken);
+    profile = await userProfil(decoded.email);
+  }
+  if (profile === "Collaborateur") {
+
+    return {
+      props: {
+        profileUser: profile,
+      },
+    };
+  } else {
+    return {
+      props: {
+        profileUser: null,
+      },
+    };
+  }
+};
+
+const pointages: NextPage = (props: any) => {
+  if (props.profileUser === "Collaborateur") {
+    return (
+      <div>
+        <Layout />
+        <form action="" method="POST" className="form-example-pointages">
+          <div className="form-example">
+            <label className="LabelPointages">
+              Semaine
+              <Datepicker
+                controls={["calendar"]}
+                select="preset-range"
+                firstSelectDay={1}
+                selectSize={7}
+                display="anchored"
+              />
+            </label>
+            {/* <input
             className="InputFormPointages"
             type="week"
             name="semainePointages"
             id="semainePointage"
           /> */}
-        </div>
-        <div className="form-example">
-          <label className="LabelPointagesHoraires">
-            Jour
-            <Datepicker
-              calendarType="week"
-              calendarSize={1}
-              display="anchored"
-            />
-          </label>
-          {/* <input
+          </div>
+          <div className="form-example">
+            <label className="LabelPointagesHoraires">
+              Jour
+              <Datepicker
+                calendarType="week"
+                calendarSize={1}
+                display="anchored"
+              />
+            </label>
+            {/* <input
             className="InputFormPointages"
             type="date"
             name="jourPointage"
             id="jourPointages"
           /> */}
-        </div>
-        <div className="form-example">
-          <label className="LabelPointagesHoraires">
-            Horaires planifié a la semaine
-          </label>
-          <input
-            className="InputFormPointages"
-            type="horairesPointages"
-            name="horairesPointages"
-            id="horairesPointages"
-            value="A modifier avec les horaires de planning"
-          />
-        </div>
-        <div className="form-example">
-          <label className="LabelPointages">Total heures travailler</label>
-          <input
-            className="InputFormPointages"
-            type="totalHorairesPointages"
-            name="totalHorairesPointages"
-            id="totalHorairesPointages"
-            value="A modifier avec les horaires de planning"
-          />
-        </div>
-        <div className="verifHoraires">
-          <div className="verifHorairesPlanning">
-            <p>Planning de la journée</p>
-            <div className="form-example-horaires">
-              <label className="LabelVerifHoraires">
-                Matin
-                <Datepicker
-                  controls={["time"]}
-                  display="bottom"
-                  touchUi={true}
-                />
-              </label>
-              {/* <input
+          </div>
+          <div className="form-example">
+            <label className="LabelPointagesHoraires">
+              Horaires planifié a la semaine
+            </label>
+            <input
+              className="InputFormPointages"
+              type="horairesPointages"
+              name="horairesPointages"
+              id="horairesPointages"
+              value="A modifier avec les horaires de planning"
+            />
+          </div>
+          <div className="form-example">
+            <label className="LabelPointages">Total heures travailler</label>
+            <input
+              className="InputFormPointages"
+              type="totalHorairesPointages"
+              name="totalHorairesPointages"
+              id="totalHorairesPointages"
+              value="A modifier avec les horaires de planning"
+            />
+          </div>
+          <div className="verifHoraires">
+            <div className="verifHorairesPlanning">
+              <p>Planning de la journée</p>
+              <div className="form-example-horaires">
+                <label className="LabelVerifHoraires">
+                  Matin
+                  <Datepicker
+                    controls={["time"]}
+                    display="bottom"
+                    touchUi={true}
+                  />
+                </label>
+                {/* <input
                 className="InputVerifHoraires"
                 type="time"
                 name="verifHorairesMatin"
                 id="verifHorairesMatin"
               /> */}
-            </div>
+              </div>
 
-            <div className="form-example-horaires">
-              <label className="LabelVerifHoraires">
-                Après-midi
-                <Datepicker
-                  controls={["time"]}
-                  display="bottom"
-                  touchUi={true}
-                />
-              </label>
-              {/* <input
+              <div className="form-example-horaires">
+                <label className="LabelVerifHoraires">
+                  Après-midi
+                  <Datepicker
+                    controls={["time"]}
+                    display="bottom"
+                    touchUi={true}
+                  />
+                </label>
+                {/* <input
                 className="InputVerifHoraires"
                 type="time"
                 name="verifHorairesApresMidi"
                 id="verifHorairesApresMidi"
               /> */}
+              </div>
             </div>
-          </div>
-          <div className="PointagesHoraires">
-            <p>Pointages</p>
-            <div className="form-example-horaires">
-              <label className="LabelPointagesHoraires">
-                Matin
-                {/* <input
+            <div className="PointagesHoraires">
+              <p>Pointages</p>
+              <div className="form-example-horaires">
+                <label className="LabelPointagesHoraires">
+                  Matin
+                  {/* <input
                 className="InputPointagesHoraires"
                 type="time"
                 name="pointagesHorairesMatin"
                 id="pointagesHorairesMatin"
               /> */}
-                <Datepicker
-                  controls={["time"]}
-                  display="bottom"
-                  touchUi={true}
-                />
-              </label>
-            </div>
+                  <Datepicker
+                    controls={["time"]}
+                    display="bottom"
+                    touchUi={true}
+                  />
+                </label>
+              </div>
 
-            <div className="form-example-horaires">
-              <label className="LabelPointagesHoraires">
-                Après-midi
-                {/* <input
+              <div className="form-example-horaires">
+                <label className="LabelPointagesHoraires">
+                  Après-midi
+                  {/* <input
                 className="InputPointagesHoraires"
                 type="time"
                 name="pointagesHorairesApresMidi"
                 id="pointagesHorairesApresMidi"
               /> */}
-                <Datepicker
-                  controls={["time"]}
-                  display="bottom"
-                  touchUi={true}
-                />
-              </label>
+                  <Datepicker
+                    controls={["time"]}
+                    display="bottom"
+                    touchUi={true}
+                  />
+                </label>
+              </div>
             </div>
-          </div>
-          <div className="PointagesHoraires">
-            <p>Correction</p>
-            <div className="form-example-horaires">
-              <label className="LabelCorrectionHoraires">
-                Matin
-                {/* <input
+            <div className="PointagesHoraires">
+              <p>Correction</p>
+              <div className="form-example-horaires">
+                <label className="LabelCorrectionHoraires">
+                  Matin
+                  {/* <input
                 className="InputCorrectionHoraires"
                 type="time"
                 name="correctionHorairesMatin"
                 id="correctionHorairesMatin"
               /> */}
-                <Datepicker
-                  controls={["time"]}
-                  display="bottom"
-                  touchUi={true}
-                />
-              </label>
-            </div>
+                  <Datepicker
+                    controls={["time"]}
+                    display="bottom"
+                    touchUi={true}
+                  />
+                </label>
+              </div>
 
-            <div className="form-example-horaires">
-              <label className="LabelCorrectionHoraires">
-                Après-midi
-                {/* <input
+              <div className="form-example-horaires">
+                <label className="LabelCorrectionHoraires">
+                  Après-midi
+                  {/* <input
                 className="InputCorrectionHoraires"
                 type="time"
                 name="correctionHorairesApresMidi"
                 id="correctionHorairesApresMidi"
               /> */}
-                <Datepicker
-                  controls={["time"]}
-                  display="bottom"
-                  touchUi={true}
-                />
-              </label>
+                  <Datepicker
+                    controls={["time"]}
+                    display="bottom"
+                    touchUi={true}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className="MotifPointages">
+              <p>Motif: </p>
+              <select id="motifs" name="motifs">
+                <option value="medicale">Médicale</option>
+                <option value="familiale">Familiale</option>
+                <option value="administratif">Administratif</option>
+              </select>
             </div>
           </div>
-          <div className="MotifPointages">
-            <p>Motif: </p>
-            <select id="motifs" name="motifs">
-              <option value="medicale">Médicale</option>
-              <option value="familiale">Familiale</option>
-              <option value="administratif">Administratif</option>
-            </select>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  } else {
+    return <PageNotFound/>
+  }
 };
 
 export default pointages;
