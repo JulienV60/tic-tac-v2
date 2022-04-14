65495459d4d97b9ef4d0a23269e30075ca31d676
Villalpando Julien <villalpando.julien@gmail.com>
2022-04-14 09:46:16 +0200

    css manager congés

--- pages/manager/conges.tsx
+++ pages/manager/conges.tsx
@@ -3,8 +3,17 @@ import LayoutManager from "../../components/LayoutManager";
 import jwt_decode from "jwt-decode";
 import { userProfil } from "../../src/userInfos";
 import PageNotFound from "../../components/PageNotFound";
+import { getDatabase } from "../../src/database";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const mongodb = await getDatabase();
+  const searchCongés = await mongodb
+    .db()
+    .collection("Collaborateurs")
+    .findOne({ profile: "Collaborateur" })
+    .then((data) => data);
+
+  console.log(searchCongés);
   const accessTokken = context.req.cookies.IdToken;
   let profile;
   let decoded: any;
@@ -28,7 +37,16 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const conges: NextPage = (props: any) => {
-  return <LayoutManager></LayoutManager>;
+  return (
+    <LayoutManager>
+      <div className="ManagerConges">
+        <div className="titreDemande">Demande de congés</div>
+        <div className="Demande">data</div>
+        <div className="titreHistorique">Historique Demande de congés</div>
+        <div className="Historique">data</div>
+      </div>
+    </LayoutManager>
+  );
 };
 
 export default conges;
