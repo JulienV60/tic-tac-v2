48112c2051b223a765b024cbe1ec5fce679ab062
mansuy maxime <mmansuy@norauto.fr>
2022-04-18 11:30:50 +0200

    test moment index collaborateur

--- pages/collaborateur/index.tsx
+++ pages/collaborateur/index.tsx
@@ -20,8 +20,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     idUser = await userId(decoded.email);
   }
   if (profile === "Collaborateur") {
-    const numeroSemaine = parseInt(moment().locale("fr").format("w")) - 2;
-    const numeroSemaineSuivante = parseInt(moment().locale("fr").format("w") +1);
+    const numeroSemaine = parseInt(moment().locale("fr").format("w")) - 1;
+    const numeroSemaineSuivante = parseInt(moment().locale("fr").format("w"));
     const mongodb = await getDatabase();
     const searchCongesActual = await mongodb
       .db()
@@ -76,7 +76,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       .collection("Collaborateurs")
       .findOne({ idUser: idUser?.toString() })
       .then((data) => data?.message);
-    const numSemaine = parseInt(moment().locale("fr").format("w")) - 2;
+    const numSemaine = parseInt(moment().locale("fr").format("w")) - 1;
 
     const contingentActuel = await mongodb
       .db()
@@ -116,7 +116,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       parseInt(sumTotalHeuresRea.toString()) -
       parseInt(sumTotalHeuresSup.toString());
 
-    const numeroJourSemaine = parseInt(moment().locale("fr").format("W")) - 2;
+    const numeroJourSemaine = parseInt(moment().locale("fr").format("W"));
 
     const searchUserConnected = await mongodb
       .db()
@@ -179,13 +179,12 @@ export default function Home(props: any) {
           {anomalie.map((element: any, index: any) => {
             if (index !== 0) {
               return (
-                <div key={index} style={{ paddingLeft: "1rem" }}>
+                <div key={index}>
                   {element.designation}
                   <br></br>
                   {element.date}
                   <br></br>
-                  {element.diff} H<br></br>
-                  {element.regulariser}
+                  {element.diff}
                 </div>
               );
             }
@@ -294,10 +293,10 @@ export default function Home(props: any) {
         <div className="ecarts">Ecarts </div>
         <div className="datacompteurs">
           {" "}
-          Contingent contractuel cumulé au {temps} : {contingentActuel} Heures / semaine moins 2 {parseInt(moment().locale("fr").format("w")) - 2}
+          Contingent contractuel cumulé au {temps} : {contingentActuel} Heures
           <br></br>
           <br></br>
-          Contingent contractuel ajusté au {temps} :{contingentCumule} Heures / semaine {parseInt(moment().locale("fr").format("w"))-1}
+          Contingent contractuel ajusté au {temps} :{contingentCumule} Heures
         </div>
         <div className="dataecarts">
           Ecart entre contingent cumulé et ajuste : {differenceCumuleActuel}{" "}
