f4ff86538d993e1a885df0c313f4409e0f2b12f1
Villalpando Julien <villalpando.julien@gmail.com>
2022-04-18 11:28:11 +0200

    css

--- pages/collaborateur/index.tsx
+++ pages/collaborateur/index.tsx
@@ -116,7 +116,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       parseInt(sumTotalHeuresRea.toString()) -
       parseInt(sumTotalHeuresSup.toString());
 
-    const numeroJourSemaine = parseInt(moment().locale("fr").format("W"));
+    const numeroJourSemaine = parseInt(moment().locale("fr").format("W")) - 2;
 
     const searchUserConnected = await mongodb
       .db()
@@ -179,12 +179,13 @@ export default function Home(props: any) {
           {anomalie.map((element: any, index: any) => {
             if (index !== 0) {
               return (
-                <div key={index}>
+                <div key={index} style={{ paddingLeft: "1rem" }}>
                   {element.designation}
                   <br></br>
                   {element.date}
                   <br></br>
-                  {element.diff}
+                  {element.diff} H<br></br>
+                  {element.regulariser}
                 </div>
               );
             }
